f123be799710225ea00dd0a11ec7e37685730832
ahmedmostafa-cell <67522336+ahmedmostafa-cell@users.noreply.github.com>
2024-02-15 02:42:40 +0200

    Upload MyProtoflioWebsite

---
+++ MyProtofolio/Controllers/HomeController.cs
@@ -0,0 +1,103 @@
+﻿using AspNetCore.SEOHelper.Sitemap;
+using AspNetCoreHero.ToastNotification.Abstractions;
+using EmailService;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyProtofolio.Controllers
+{
+    public class HomeController : Controller
+    {
+        IEmailSender _emailSender;
+        private readonly INotyfService _notyf;
+        private readonly IWebHostEnvironment _env;
+        public HomeController(IEmailSender emailSender , INotyfService notyf , IWebHostEnvironment env)
+        {
+
+            _emailSender = emailSender;
+            _notyf = notyf;
+            _env = env;
+        }
+
+        public async Task<IActionResult> IndexAsync(string name, string email, string message, IFormCollection form, IFormFileCollection files)
+        {
+            try
+            {
+                if (name != null)
+                {
+                    var userEmail = email;
+
+                    var messages = new Message(new string[] { "ahmedmostafa706@gmail.com" }, "Email From Customer " + email, "This is the content from our async email. i am happy", files);
+                    //var filePaths = Path.Combine(Directory.GetCurrentDirectory(), @"wwwroot\Uploads", ImageName);
+                    //var files = Request.Form.Files.Any() ? Request.Form.Files : new FormFileCollection();
+                    await _emailSender.SendEmailAsync(messages, message);
+                     _notyf.Success("The Message Has Been Sent");
+                    return View();
+                }
+                else
+                {
+                    return View();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                ViewBag.ex = ex;
+                return View();
+
+            }
+
+
+        }
+
+
+        public async Task<IActionResult> ArabicIndexAsync(string name, string email, string message, IFormCollection form, IFormFileCollection files)
+        {
+            if (name != null)
+            {
+                var userEmail = email;
+
+                var messages = new Message(new string[] {"ahmedmostafa706@gmail.com"}, "Email From Customer " + email, "This is the content from our async email. i am happy", files);
+                //var filePaths = Path.Combine(Directory.GetCurrentDirectory(), @"wwwroot\Uploads", ImageName);
+                //var files = Request.Form.Files.Any() ? Request.Form.Files : new FormFileCollection();
+                await _emailSender.SendEmailAsync(messages, message);
+                return View();
+            }
+            else
+            {
+                return View();
+            }
+
+        }
+        public IActionResult Blog()
+        {
+            return View();
+        }
+        public IActionResult MesageSent()
+        {
+            _notyf.Success("The Message Has Been Sent");
+            return View();
+        }
+
+
+        //not returning any view  
+        public string CreateSitemapInRootDirectory()
+        {
+            var list = new List<SitemapNode>();
+            list.Add(new SitemapNode { LastModified = DateTime.UtcNow, Priority = 0.8, Url = "https://ahmedattya.com/", Frequency = SitemapFrequency.Daily });
+            list.Add(new SitemapNode { LastModified = DateTime.UtcNow, Priority = 0.8, Url = "https://ahmedattya.com/", Frequency = SitemapFrequency.Yearly });
+            list.Add(new SitemapNode { LastModified = DateTime.UtcNow, Priority = 0.7, Url = "https://ahmedattya.com/", Frequency = SitemapFrequency.Monthly });
+
+            new SitemapDocument().CreateSitemapXML(list, _env.ContentRootPath);
+            return "sitemap.xml file should be create in root directory";
+        }
+
+
+
+    }
+}
---
+++ MyProtofolio/Startup.cs
@@ -0,0 +1,84 @@
+using AspNetCore.SEOHelper;
+using AspNetCoreHero.ToastNotification;
+using EmailService;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyProtofolio
+{
+    public class Startup
+    {
+        IConfiguration config;
+        public Startup(IConfiguration configuration)
+        {
+            config = configuration;
+        }
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            
+           
+            var emailConfig = config
+              .GetSection("EmailConfiguration")
+              .Get<EmailConfiguration>();
+            services.AddSingleton(emailConfig);
+            services.AddScoped<IEmailSender, EmailSender>();
+            services.AddNotyf(config =>
+            {
+                config.DurationInSeconds = 10;
+                config.IsDismissable = true;
+                config.Position = NotyfPosition.TopCenter;
+            });
+            services.AddControllersWithViews();
+
+            services.AddDistributedMemoryCache();
+            services.AddSession();
+            services.AddHttpContextAccessor();
+
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseStaticFiles();
+            app.UseXMLSitemap(env.ContentRootPath);
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+
+                endpoints.MapControllerRoute(
+
+                  name: "areas",
+                  pattern: "{area:exists}/{controller=Home}/{action=Index}");
+
+
+                endpoints.MapControllerRoute(
+
+                     name: "default",
+                     pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+
+
+
+            });
+        }
+    }
+}
